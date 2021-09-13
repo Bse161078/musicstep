a922f746e7021988e53abc6789377591403b5a00
Asad Ullah <asadullah.shaheen@gmail.com>
2021-09-12 22:42:16 -0700

    fixes responsivness of trial page

--- src/components/StartTrialWrapper/StartTrialWrapper.style.ts
+++ src/components/StartTrialWrapper/StartTrialWrapper.style.ts
@@ -3,15 +3,23 @@ import styled from "styled-components"
 export const StartTrialWrapperStyle = styled.section`
     display: grid;
     grid-template-columns: 2fr 1fr;
-    grid-column-gap: 30px;
+    grid-gap: 30px;
     padding: 30px;
     background: #F7F7F7;
     margin: 100px 50px;
     border-radius: 32px;
 
+    @media( max-width: 767px ) {
+        grid-template-columns: 1fr;
+    }
+
     .left-section {
         display: grid;
         grid-row-gap: 30px;
+
+        @media ( max-width: 767px ) {
+            display: none;
+        }
 
         .left-thumb {
             max-width: 100%;
