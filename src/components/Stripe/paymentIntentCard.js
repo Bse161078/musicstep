1bf8bcce6dd2c80c163c255133475ae343ec0102
Muhammad <hamza_satti@live.com>
2022-06-07 11:52:58 +0500

    push

--- src/components/Stripe/paymentIntentCard.js
+++ src/components/Stripe/paymentIntentCard.js
@@ -61,7 +61,7 @@ export default function PaymentIntentCard() {
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: "https://music-pass-frontend.web.app/credit-payment-success",
+                return_url: "http://50.16.77.114/credit-payment-success",
             },
         });
 
