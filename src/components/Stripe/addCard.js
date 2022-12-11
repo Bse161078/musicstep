d453eace0f9f736fdcbddeb733fa60856d7053f0
abdullahzahid321456 <abdullahzahid321456@gmail.com>
2022-12-11 19:55:25 +0500

    before pull

--- src/components/Stripe/addCard.js
+++ src/components/Stripe/addCard.js
@@ -85,7 +85,7 @@ export default function AddCard(props) {
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-               return_url: props.isUpdateSubscription?`${process.env.SITE_URL}/update-subscription`:`${process.env.SITE_URL}/free-trial`,
+               return_url: props.isUpdateSubscription?`${process.env.REACT_APP_PUBLIC_URL}/update-subscription`:`${process.env.REACT_APP_PUBLIC_URL}/free-trial`,
 
             },
         });
